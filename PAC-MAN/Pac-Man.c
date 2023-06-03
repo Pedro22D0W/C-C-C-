f4d4ff61689d5b50096dbcd862c9574813a9344c
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2023-06-03 10:40:33 -0300

    correção da passagem de parametro

--- PAC-MAN/Pac-Man.c
+++ PAC-MAN/Pac-Man.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "conio.h"
 #include "pac-man.h"
-MP m;
+
+
+
 
 
 
@@ -9,132 +12,32 @@ MP m;
 
 int main() {
   char tecla;
+  MP m;
+ int x_pac;
+ int y_pac;
 
 
 
-aloca_mapa();
+aloca_mapa(&m);
+acha_personagem(m,&x_pac,&y_pac);
+
 
  do
  {
     
 
-    scanf(" %c",&tecla);
-    controle(tecla);
+    //scanf(" %c",&tecla);
+    tecla =getch();
+    controle(&m,tecla,&x_pac,&y_pac);
+    system("cls");
 
      for (int i = 0; i < 13; i++) {
      printf("%s\n", m.map[i]);
   };
 
   } while (!acabou());
- libera_memoria();
+ libera_memoria(&m);
  
 
  }
   
-
-
-
-
-
-
-void controle(char tecla){
- int x_pac;
- int y_pac;
-
-/////////////acha posição do personagem na matriz///////////
- for (int i = 0; i < m.linhas; i++)
- {
-      for (int j = 0; j < m.colunas; j++)
-      {
-          if (m.map[i][j] == '@')
-          {
-            x_pac =i;
-            y_pac =j;
-            break;
-          }
-        } 
-}
-/////////////acha posição do personagem na matriz///////////
-
-/////////////move personagem///////////////////////////////
-
-switch (tecla)
-{
-case 'a':
-  m.map[x_pac][y_pac -1] = '@';
-  break;
-
-case 's':
-  m.map[x_pac +1 ][y_pac] = '@';
-  break;
-case 'd':
-  m.map[x_pac][y_pac +1] = '@';
-  break;
-
-  case 'w':
-  m.map[x_pac -1 ][y_pac] = '@';
- 
-  break;
-}
-m.map[x_pac][y_pac]= '.';
-}
-
-int acabou(){
-  return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void libera_memoria(){
-
-  
-  
-for (int i = 0; i < m.linhas; i++)      //<---------------(libera a memoria)
-{
-  free(m.map[i]);
-}
-  free(m.map);                          //<---------------(libera a memoria)
-
-    };
-
-void aloca_mapa(){
-  FILE *f; 
-
-
- f = fopen("../mapa.txt", "r");                    //<---------------(lê arqivo para definir o numero de colunas e linhas)
- fscanf(f,"%d %d",&m.linhas,&m.colunas);
-
-  ////////// aloca mapa dinamicamente //////////////////
-m.map = malloc(sizeof(char*) * m.linhas);
-for (int i = 0; i < m.linhas; i++)
-{
-  m.map[i] = malloc(sizeof(char) * (m.colunas +1 ));
-  
-}
-///////////////////////////////////////////////////////
-//////////////exibe mapa///////////////////////////
-  for (int i = 0; i < m.linhas; i++) {
-    fscanf(f, "%s", m.map[i]);
-  }
-
-  for (int i = 0; i < m.linhas; i++) {
-    printf("%s\n", m.map[i]);
-  };
-
-  fclose(f);                          //<---------------(fecha arquivo)
-  /////////////////////////////////////////////////
-}
--- PAC-MAN/pac-man.h
+++ PAC-MAN/pac-man.h
@@ -1,8 +1,3 @@
-void libera_memoria();
-void aloca_mapa();
-int acabou();
-void controle(char tecla);
-
 struct mapa
 {
 int linhas;
@@ -11,3 +6,139 @@ char ** map;
 };
 
 typedef struct mapa MP;
+
+void libera_memoria(MP* m);
+void aloca_mapa(MP* m);
+int acabou();
+void controle(MP *m,char tecla,int *x_pac,int*y_pac);
+void acha_personagem(MP m,int *x_pac,int*y_pac);
+
+/////////////move personagem///////////////////////////////
+void controle(MP *m,char tecla,int *x_pac,int*y_pac){
+
+
+switch (tecla)
+{
+case 'a':
+if ((*m).map[*x_pac][*y_pac -1] != '-' && (*m).map[*x_pac][*y_pac -1] != '|' && (*m).map[*x_pac][*y_pac -1] != '_' )
+{
+  (*m).map[*x_pac][*y_pac -1] =  '@';
+  (*m).map[*x_pac][*y_pac]= '.';
+  (*y_pac) -= 1;
+}
+break;
+case 's':
+if ((*m).map[*x_pac +1 ][*y_pac] != '|' && (*m).map[*x_pac + 1][*y_pac] != '-' && (*m).map[*x_pac + 1][*y_pac] != '_')
+{
+  (*m).map[*x_pac +1 ][*y_pac] = '@';
+  (*m).map[*x_pac][*y_pac]= '.';
+  (*x_pac) +=1;
+}
+  break;
+
+case 'd':
+if ((*m).map[*x_pac][*y_pac +1] != '-' && (*m).map[*x_pac][*y_pac +1] != '|' && (*m).map[*x_pac][*y_pac +1] != '_')
+{
+  (*m).map[*x_pac][*y_pac +1] =  '@';
+  (*m).map[*x_pac][*y_pac]= '.';
+  (*y_pac) +=1;
+}
+  break;
+
+  case 'w':
+  if ((*m).map[*x_pac -1 ][*y_pac] != '|' && (*m).map[*x_pac - 1][*y_pac] != '-' && (*m).map[*x_pac -1 ][*y_pac] != '_')
+{
+  (*m).map[*x_pac -1 ][*y_pac] = '@';
+  (*m).map[*x_pac][*y_pac]= '.';
+  (*x_pac) -=1;
+}
+  break;
+}
+
+
+}
+/////////////move personagem///////////////////////////////
+
+int acabou(){
+  return 0;
+}
+
+void aloca_mapa(MP* m ){
+  FILE *f; 
+
+
+ f = fopen("../mapa.txt", "r");                    //<---------------(lê arqivo para definir o numero de colunas e linhas)
+ fscanf(f,"%d %d",&(*m).linhas,&(*m).colunas);
+
+  ////////// aloca mapa dinamicamente //////////////////
+(*m).map = malloc(sizeof(char*) * (*m).linhas);
+for (int i = 0; i <(*m).linhas; i++)
+{
+  (*m).map[i] = malloc(sizeof(char) * ((*m).colunas +1 ));
+  
+}
+///////////////////////////////////////////////////////
+//////////////exibe mapa///////////////////////////
+  for (int i = 0; i <(*m).linhas; i++) {
+    fscanf(f, "%s", (*m).map[i]);
+  }
+
+  for (int i = 0; i <(*m).linhas; i++) {
+    printf("%s\n", (*m).map[i]);
+  };
+
+  fclose(f);                          //<---------------(fecha arquivo)
+  /////////////////////////////////////////////////
+}
+
+void libera_memoria(MP* m){
+
+  
+  
+for (int i = 0; i < (*m).linhas; i++)      //<---------------(libera a memoria)
+{
+  free((*m).map[i]);
+}
+  free((*m).map);                          //<---------------(libera a memoria)
+
+    };
+
+
+/////////////acha posição do personagem na matriz///////////
+void acha_personagem(MP m,int *x_pac,int*y_pac){
+
+ for (int i = 0; i < m.linhas; i++)
+ {
+      for (int j = 0; j < m.colunas; j++)
+      {
+          if (m.map[i][j] == '@')
+          {
+            (*x_pac) =i;
+            (*y_pac) =j;
+            break;
+          }
+        } 
+}
+};
+/////////////acha posição do personagem na matriz///////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
